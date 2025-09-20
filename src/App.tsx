1c0c73a9a5e2c289153e34625840bc95ddeeac4f
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-09-20 21:01:14 +0900

    TODOリストの表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import "./App.css";
 
 function App() {
+  const todos = [
+    { id: 1, title: "買い物", done: false },
+    { id: 2, title: "掃除", done: true },
+    { id: 3, title: "散歩", done: false },
+  ];
   return (
     <>
       <div>
@@ -10,6 +15,19 @@ function App() {
         >
           追加
         </button>
+        {todos.map((todo) => (
+          <div key={todo.id} className="flex items-center mt-4">
+            <input
+              type="checkbox"
+              checked={todo.done}
+              onChange={() => {}}
+              className="mr-2"
+            />
+            <span className={todo.done ? "line-through" : ""}>
+              {todo.title}
+            </span>
+          </div>
+        ))}
       </div>
     </>
   );
