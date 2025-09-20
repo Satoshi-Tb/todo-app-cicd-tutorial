c0a676ebee589db6884a503ba6059ed1f10ca381
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-09-20 21:20:28 +0900

    stateを導入し、TODOを追加できるようにする。

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
+type TodoType = { id: number; title: string; done: boolean };
+
+const initialTodos: TodoType[] = [
+  { id: 1, title: "買い物", done: false },
+  { id: 2, title: "掃除", done: true },
+  { id: 3, title: "散歩", done: false },
+];
+
 function App() {
-  const todos = [
-    { id: 1, title: "買い物", done: false },
-    { id: 2, title: "掃除", done: true },
-    { id: 3, title: "散歩", done: false },
-  ];
+  const [todoTitle, setTodoTitle] = useState("");
+  const [todos, setTodos] = useState<TodoType[]>(initialTodos);
+
   return (
     <>
       <div>
@@ -14,7 +21,13 @@ function App() {
             <input
               type="checkbox"
               checked={todo.done}
-              onChange={() => {}}
+              onChange={() => {
+                setTodos((prevTodos) =>
+                  prevTodos.map((t) =>
+                    t.id === todo.id ? { ...t, done: !t.done } : t
+                  )
+                );
+              }}
               className="mr-2"
             />
             <span className={todo.done ? "line-through" : ""}>
@@ -22,11 +35,29 @@ function App() {
             </span>
           </div>
         ))}
-        <form>
+        <form
+          className="mt-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (todoTitle.trim() === "") return;
+
+            setTodos((prevTodos) => {
+              const newTodo: TodoType = {
+                id: prevTodos.length + 1,
+                title: todoTitle,
+                done: false,
+              };
+              return [...prevTodos, newTodo];
+            });
+            setTodoTitle("");
+          }}
+        >
           <input
             type="text"
             placeholder="追加するタスク"
             className="border border-gray-300 rounded px-2 py-1 mt-4"
+            value={todoTitle}
+            onChange={(e) => setTodoTitle(e.target.value)}
           />
           <button
             type="submit"
