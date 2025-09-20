4a2253af749160684d16106ed1e3ce9678de3aa9
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-09-21 00:37:11 +0900

    TODO削除追加

--- src/App.tsx
+++ src/App.tsx
@@ -75,10 +75,22 @@ const TodoList = React.memo(({ todos, setTodos }: TodoListProps) => {
     [setTodos]
   );
 
+  const handleTodoDelete = useCallback(
+    (id: number) => {
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    },
+    [setTodos]
+  );
+
   return (
     <div>
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} handleDone={handleTodoDone} />
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleDone={handleTodoDone}
+          handleDelete={handleTodoDelete}
+        />
       ))}
     </div>
   );
@@ -87,28 +99,41 @@ const TodoList = React.memo(({ todos, setTodos }: TodoListProps) => {
 type TodoItemProps = {
   todo: TodoType;
   handleDone: (id: number) => void;
+  handleDelete: (id: number) => void;
 };
 
-const TodoItem = React.memo(({ todo, handleDone }: TodoItemProps) => {
-  // useEffect(() => {
-  //   console.log("レンダリングTODO ITEM", todo.id);
-  // });
+const TodoItem = React.memo(
+  ({ todo, handleDone, handleDelete }: TodoItemProps) => {
+    // useEffect(() => {
+    //   console.log("レンダリングTODO ITEM", todo.id);
+    // });
 
-  const handleTodoDone = useCallback(() => {
-    handleDone(todo.id);
-  }, [handleDone, todo.id]);
+    const handleTodoDone = useCallback(() => {
+      handleDone(todo.id);
+    }, [handleDone, todo.id]);
 
-  return (
-    <div className="flex items-center mt-4">
-      <input
-        type="checkbox"
-        checked={todo.done}
-        onChange={handleTodoDone}
-        className="mr-2"
-      />
-      <span className={todo.done ? "line-through" : ""}>{todo.title}</span>
-    </div>
-  );
-});
+    const handleTodoDelete = useCallback(() => {
+      handleDelete(todo.id);
+    }, [handleDelete, todo.id]);
+
+    return (
+      <div className="flex items-center mt-4">
+        <input
+          type="checkbox"
+          checked={todo.done}
+          onChange={handleTodoDone}
+          className="mr-2"
+        />
+        <span className={todo.done ? "line-through" : ""}>{todo.title}</span>
+        <button
+          className="ml-auto bg-red-500 text-white rounded px-2 py-1"
+          onClick={handleTodoDelete}
+        >
+          削除
+        </button>
+      </div>
+    );
+  }
+);
 
 export default App;
